11eb395f369fcdc2aa7aacc1b6c3164fe85a97f9
Leonardo Pica <leonardoarielpica@gmail.com>
2026-04-26 19:23:06 -0300

    Cambios varios y pruebas

--- Tetris/main.c
+++ Tetris/main.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "funciones.h"
+//#include "graficos.h"
+//#include "input.h"
+//#include "juego.h"
+#include "gbt.h"
 
-int main()
+/*
+Apellido y nombre: Abrate, Micaela Abril
+DNI: 42319879
+Usuario: MicaAbrate
+Entrega: Sí
+
+Apellido y nombre: Engler, Matías Ezequiel
+DNI: 43316397
+Usuario: me-engler
+Entrega: Sí
+
+Apellido y nombre: Pica, Leonardo Ariel
+DNI: 43083209
+Usuario: leoopica
+Entrega: Sí
+*/
+
+int main ()
 {
-    printf("Tetris");
+    gbt_iniciar();
+
+    gbt_crear_ventana("Tetris", 640, 480, 1);
+
+    while (1)
+    {
+
+    }
+
     return 0;
 }
