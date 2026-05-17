8eda37d6c022504925005826f357e00d6ab5bd71
Leonardo Pica <leonardoarielpica@gmail.com>
2026-05-17 00:12:54 -0300

    Colores, centrado de pantallas - Archivo faltante

--- archivos.h
+++ archivos.h
@@ -1,8 +1,37 @@
-//
-// Created by Mati on 8/5/2026.
-//
+#ifndef ARCHIVOS_H_INCLUDED
+#define ARCHIVOS_H_INCLUDED
 
-#ifndef TRABAJO_PRACTICO_TOPICOS_TETRIS_ARCHIVOS_H
-#define TRABAJO_PRACTICO_TOPICOS_TETRIS_ARCHIVOS_H
+#include "funcionalidades.h"
+#include "configuracion.h"
 
-#endif //TRABAJO_PRACTICO_TOPICOS_TETRIS_ARCHIVOS_H
+#define NOMBRE_ARCHIVO_PARTIDA "partida.dat"
+
+// Estructura que guarda el estado completo de una partida
+typedef struct {
+    int tablero[filasTablero][columnasTablero];
+    sPieza actual;
+    sPieza proxima;
+    int puntaje;
+    int nivel;
+    int lineas_totales;
+    int piezas_caidas;
+    double duracion_caida;
+    char nombreJugador[21];
+    int bolsa[7];
+    int bolsaIndice;
+    int valido; // 1 si hay partida guardada
+} sEstadoPartida;
+
+// Devuelve 1 si existe una partida guardada válida
+int PARTIDA_EXISTE(void);
+
+// Guarda la partida actual en el archivo binario. Retorna 0 si OK, -1 si error.
+int PARTIDA_GUARDAR(void);
+
+// Carga la partida desde el archivo binario. Retorna 0 si OK, -1 si error.
+int PARTIDA_CARGAR(void);
+
+// Borra el archivo de partida guardada
+void PARTIDA_BORRAR(void);
+
+#endif // ARCHIVOS_H_INCLUDED
